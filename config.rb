e6d06a5383e9f9aaecc6f7c33de04d58688112b6
Lucas Torri <lucastorri@gmail.com>
2012-06-15 22:53:20 -0300

    adding pretty urls

--- config.rb
+++ config.rb
@@ -68,6 +68,8 @@
 # Change the images directory
 # set :images_dir, "alternative_image_directory"
 
+activate :directory_indexes
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
